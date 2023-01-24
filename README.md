84c473d159fa58d5fce8e715e4a2cb35d0cc8384
ANGEL GUERRA <122810612+GUERRA-ANGEL@users.noreply.github.com>
2023-01-24 19:40:44 +0000

    Se creo el archivo programa1.py

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
-#Curso de Programación orientada a objetos
-##Índice
+# Curso de Programación orientada a objetos
+## Índice
 |No1.|Archivo|Descripción|
 |--|--|--|
 |1.|programa0.py|Hola mundo en pyton|
+|2.|programa1.py|comentarios multilinea,comentarios de una linea y concatenacion de cadenas|
---
+++ semana2/programa1.py
@@ -0,0 +1,13 @@
+"""
+    programa1
+    nombre: Angel G.M.
+    fecha: 23/01/2023
+    descripcion:comentario multilínea, comentarios de una línea y concatenación de cadenas
+"""
+variable1 = "Hola"  # variable que almacena una cadea de caracteres
+variable2 = "Python"  # Variable que almacena una cadena de caracteres
+print(variable1 + variable2)  # concatenar 2 cadenas
+"""
+variable1 = "Hola" # varibale que almacena cadena de caracteres 
+variable2 = "619" # variable que almacena cadena de caracteres
+print(variable1 + variable2) # imprima la concatenacion de 2 variables de tipo str

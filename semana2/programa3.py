7a4b778f11117ee99e3b5430fdda222f593f8b97
ANGEL GUERRA <122810612+GUERRA-ANGEL@users.noreply.github.com>
2023-01-24 20:47:03 +0000

    se creo el archivo creado programa3.py

---
+++ semana2/programa3.py
@@ -0,0 +1,19 @@
+"""
+   programa3
+   Nombre:
+   Fecha:
+   Descripcion:
+"""
+numero1=10
+numero2=5
+print(numero1 + numero2)
+print("{}+{}={}".format(numero1,numero2,(numero1+numero2)))
+
+numero3 = 5
+numero4 = 10
+print(numero3*numero4)
+print("{}={}*{}".format((numero3 * numero4),numero3,numero4))
+
+numero5 =10
+numero6 =5
+print(numero5/numero6)

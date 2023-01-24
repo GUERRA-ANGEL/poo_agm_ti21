007be581c5134a7de4fcc7d2cc51037946855e06
ANGEL GUERRA <122810612+GUERRA-ANGEL@users.noreply.github.com>
2023-01-24 20:00:03 +0000

    concatenacion usando la funcion format

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 |--|--|--|
 |1.|programa0.py|Hola mundo en pyton|
 |2.|programa1.py|comentarios multilinea,comentarios de una linea y concatenacion de cadenas|
+|3.|programa2.py|concatenacion usando la funcion format|
---
+++ semana2/programa2.py
@@ -0,0 +1,9 @@
+"""
+   programa2
+   Nombre:Angel GM
+   Fecha:24/01/2023
+   Descripción:concatenacion usando la funcion format
+"""
+variable1 = "Hola" #variable tipo
+variable2 = 619 #variable tipo int
+print("{} {}".format(variable1,variable2))

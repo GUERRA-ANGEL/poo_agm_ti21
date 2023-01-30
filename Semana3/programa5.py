a5daa98e786cf27ca621d6740a6957cc1acb5a65
ANGEL GUERRA <122810612+GUERRA-ANGEL@users.noreply.github.com>
2023-01-30 16:57:18 +0000

    Creo un programa que haga suma,resta,multiplicacion,disivion,potencia

---
+++ Semana3/programa5.py
@@ -0,0 +1,14 @@
+numero1 = input("Numero1: ")
+numero2 = 10
+suma = int(numero1) + numero2
+print(suma)
+
+numero1 = input("numero1 : ")
+numero2 = input("numero2 : ")
+print(int(numero1) - numero2)
+print(int(numero1) * numero2)
+print(int(numero1) ^ numero2)
+print(resta)
+print(multiplicacion)
+print(division)
+print(potencia)

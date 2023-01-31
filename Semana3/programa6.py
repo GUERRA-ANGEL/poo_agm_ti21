e2daec7cc3e66d583659ff0809f10b2f5ce96fda
ANGEL GUERRA <122810612+GUERRA-ANGEL@users.noreply.github.com>
2023-01-31 04:25:00 +0000

    Crear una aplicación que permita calcular el área y perímetro de cualquier triangulo.

---
+++ Semana3/programa6.py
@@ -0,0 +1,9 @@
+lado1 = (input("lado1: "))
+lado2 = (input("lado2: "))
+lado3 = (input("lado3: "))
+base = int(input("base: "))
+altura = int(input("altura: "))
+
+p = (lado1 + lado2 + lado3)
+a = (base * altura)/2
+print("El perimetro del triangulo es {} y el area del triangulo es: {}".format(p,a))

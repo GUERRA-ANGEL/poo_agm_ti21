4527e15bb643451d1043c34e100fb4b8fc8bf871
ANGEL GUERRA <122810612+GUERRA-ANGEL@users.noreply.github.com>
2023-01-31 20:39:26 +0000

    Calcular el área y perímetro de circulo y cuadrado

--- Semana3/programa6.py
+++ Semana3/programa6.py
@@ -1,8 +1,8 @@
 lado1 = (input("lado1: "))
 lado2 = (input("lado2: "))
 lado3 = (input("lado3: "))
-base = int(input("base: "))
-altura = int(input("altura: "))
+base = int(input("base: ")) #int sirve para agregar valores enteros
+altura = int(input("altura: ")) # inputPara agregar una cantidad 
 
 p = (lado1 + lado2 + lado3)
 a = (base * altura)/2
---
+++ Semana3/programa7.py
@@ -0,0 +1,22 @@
+"""
+   programa7
+   Nombre:Angel Guerra Muñoz
+   Fecha:31/01/2023
+   Descripcion:Calcular el area y perimetro de circulo y cuadrado
+"""   
+lado1 = float(input("lado1: "))
+lado2 = float(input("lado2: "))
+lado3 = float(input("lado3: "))
+lado4 = float(input("lado4: "))
+a = lado1**2
+p = lado1 + lado2 + lado3 + lado4
+print("El area del cuadrado es ={}".format(a))
+print("El perimetro es ={}".format(p))
+
+radio = float(input("radio: "))
+area = float( input("area: "))
+perimetro = float(input("perimetro: "))
+radio =
+area =
+perimetro =
+print

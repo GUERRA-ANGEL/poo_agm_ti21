cce58133f74d78ead8891b58d5edee4b3af1b2a6
ANGEL GUERRA <122810612+GUERRA-ANGEL@users.noreply.github.com>
2023-02-04 05:56:27 +0000

    Crear calculadora

---
+++ Reto/programa9.py
@@ -0,0 +1,11 @@
+Num1 = float(input("Digite numero1: "))
+Num2 = float(input("Digite numero2: "))
+
+sumar = Num1 + Num2
+print ("La suma de ={}".format(Num1+Num2))
+restar = Num1 - Num2
+print("La resta de ={}".format(Num1-Num2))
+multiplicar = Num1 * Num2
+print("La multiplicacion de ={}".format(Num1*Num2))
+division = Num1 / Num2
+print("La division de ={}".format(Num1/Num2))

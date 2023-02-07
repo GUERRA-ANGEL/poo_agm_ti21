0bb13cd9e488dfe6fa6fca6eaef4cb7556ca9ff6
ANGEL GUERRA <122810612+GUERRA-ANGEL@users.noreply.github.com>
2023-02-07 20:56:18 +0000

    Crear programa de 2 numeros

---
+++ semana4/programa13.py
@@ -0,0 +1,73 @@
+"""
+programa13.py
+Nombre:Angel Guerra Muñoz
+Fecha:07/02/2023
+Descripcion:
+"""
+
+n1 = int(input("Numero1"))
+n2 = int(input("Numero2"))
+
+#Forma one
+if n1>n2:
+    print(n1)
+if n2<n1:
+    print(n2)
+if n1==n2:
+    print(None)
+    
+#Forma two
+if n2>n1:
+    print(n2)
+if n1<n2:
+    print(n1)
+else:
+    print(None) 
+    
+# Forma three     
+if n1>n2:
+    print(n1)
+elif n2>n1:
+    print(n2)
+else:
+    print(None)
+    
+# Forma four
+if n1==n2:
+    print(None)
+elif n1>n2:
+    print(n1)
+elif n2>n1:
+    print(n2)
+    
+# Forma five
+if n1<n2:
+    print(n2)
+if n2<n1:
+    print(n1)
+if n1==n2:
+    print(None)
+    
+# Forma six
+if n2>n1:
+    print(n2)
+if n2<n1:
+    print(n1)
+else:
+    print(None)
+    
+# Forma seven
+if(n2<n1>n2):
+    print(n1)
+elif(n1<n2>n1):
+    print(n2)
+else:
+    print(None)
+    
+# Forma eigth
+if n1<=n2:
+    print(n1)
+if n1==n2:
+    print(None)
+else:
+    print(n2)

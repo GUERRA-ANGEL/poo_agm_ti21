627c2cd7139a23d296747e935132059cb935e6a0
ANGEL GUERRA <122810612+GUERRA-ANGEL@users.noreply.github.com>
2023-02-09 20:43:33 +0000

    Forma 12

---
+++ semana4/programa17.py
@@ -0,0 +1,24 @@
+"""
+    programa17.py
+    nombre: Angel G.M.
+    fecha: 09/02/2023
+    descripcion: Forma 12
+"""
+
+def mayor(numero1,numero2):
+    result =  None
+    if numero1 > numero2:
+        result = numero1
+    elif numero2 > numero1:
+        result = numero2
+
+    return result
+print(mayor(2,1)) #2
+print(mayor(1,2)) #2
+print(mayor(2,2)) #None
+print(mayor(2,-1)) #2
+print(mayor(-1,2)) #2
+print(mayor(-1,-1)) #None
+print(mayor(-2,-1)) #-1
+print(mayor(-1,-2)) #-1
+
---
+++ semana4/programa18.py
@@ -0,0 +1,23 @@
+"""
+    programa18.py
+    nombre: Angel G.M.
+    fecha: 09/02/2023
+    descripcion: Forma 12
+"""
+
+def mayor(numero1:int,numero2:int)->int | None:
+    result = None
+    if numero1 > numero2:
+        result = numero1
+    elif numero2 > numero1:
+        result = numero2
+    return result
+ 
+print(mayor(2,1)) #2
+print(mayor(1,2)) #2
+print(mayor(2,2)) #None
+print(mayor(2,-1)) #2
+print(mayor(-1,2)) #2
+print(mayor(-1,-1)) #None
+print(mayor(-2,-1)) #-1
+print(mayor(-1,-2)) #-1

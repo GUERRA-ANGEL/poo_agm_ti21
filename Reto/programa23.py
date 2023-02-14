dab014bc4fa99dafc706f6153dcf5d9775174a93
ANGEL GUERRA <122810612+GUERRA-ANGEL@users.noreply.github.com>
2023-02-14 03:39:58 +0000

    Realiza la accion que al ingresar 1 imprime true y cuando se ingresa 0 imprime false

---
+++ Reto/programa23.py
@@ -0,0 +1,13 @@
+"""
+   programa21.py
+   Nombre:Angel Guerra Muñoz
+   Fecha:13/02/2023
+   Descripcion:Realiza la accion que al ingresar 1 imprime true y cuando se ingresa 0 imprime false
+"""  
+
+entrada = int(input("Ingrese un número(0 o 1): "))
+if entrada == 1:
+    print("True")
+else:
+    print("False")
+    

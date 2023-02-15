579ce8131926168234c36e4fed76153cf30e5aa0
ANGEL GUERRA <122810612+GUERRA-ANGEL@users.noreply.github.com>
2023-02-15 05:07:41 +0000

    Realiza restas

---
+++ Reto/programa24.py
@@ -0,0 +1,11 @@
+"""
+   programa24.py
+   Nombre:Angel Guerra Muñoz
+   Fecha:14/02/2023
+   Descripcion:Realiza restas
+""" 
+
+a = float(input("Ingrese numero mayor: "))
+b = float(input("Ingrese numero menor: "))
+x = a-b
+print(x)

1a87583e57ee54a6434c504973c3d4b7e6af6dda
ANGEL GUERRA <122810612+GUERRA-ANGEL@users.noreply.github.com>
2023-02-18 03:51:39 +0000

    Imprime Los dinosaurios se existieron hace mas de 100 millones de años

---
+++ Reto/programa27.py
@@ -0,0 +1,10 @@
+"""
+   programa27.py
+   Nombre:Angel Guerra Muñoz
+   Fecha:17/02/2023
+   Descripcion:imprime Los dinosaurios se existieron hace mas de 100 millones de años
+""" 
+
+
+
+print("Los dinosaurios se existieron hace mas de 100 millones de años")

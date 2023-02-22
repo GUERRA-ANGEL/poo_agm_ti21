20f53dc7ea0e9901d214d2d44eca6f41f78cd5e8
ANGEL GUERRA <122810612+GUERRA-ANGEL@users.noreply.github.com>
2023-02-22 05:10:30 +0000

    Crea nombre,peso,longitud de un dinosaurio

---
+++ Reto/programa31.py
@@ -0,0 +1,18 @@
+"""
+   programa31.py
+   Nombre:Angel Guerra Muñoz
+   Fecha:21/02/2023
+   Descripcion:Crea nombre,peso,longitud de un dinosaurio
+""" 
+
+nom = input("Ingrese nombre de dinosaurio: ")
+Pekg = float(input("Ingrese peso en kilogramos: "))
+Lon = float(input("Ingrese longitud en metros: "))
+
+Pekg = Pekg*0.453592
+Lon = Lon*0.3047
+
+print(nom)
+print(Pekg)
+print(Lon)
+

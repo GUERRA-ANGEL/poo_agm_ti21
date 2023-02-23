051cdddd110d84a9299e4cd4380e502edadda925
ANGEL GUERRA <122810612+GUERRA-ANGEL@users.noreply.github.com>
2023-02-23 05:30:39 +0000

    Calcula el cuadrado y cubo de un numero

---
+++ Reto/programa32.py
@@ -0,0 +1,14 @@
+"""
+   programa31.py
+   Nombre:Angel Guerra Muñoz
+   Fecha:22/02/2023
+   Descripcion:Calcula el cuadrado y cubo de un numero
+""" 
+
+Num = float(input("Insertar numero: ")) #Insertar un numero
+
+cuadrado = Num**2    #Operacion de cuadrado
+cubo = Num**3        #Operacion de cubo
+
+print("El numero al cuadrado es : ",cuadrado)    #Imprime el numero al cuadrado es y el resultado
+print("El numero al cubo es: ",cubo)             #Imprime el numero al cubo es y el restado

22f767a2e74aa9fd30aa11401ef41e1b1e3966c9
ANGEL GUERRA <122810612+GUERRA-ANGEL@users.noreply.github.com>
2023-02-24 05:23:52 +0000

    Calcula el are y volumen de un hexaedro

---
+++ Reto/programa33.py
@@ -0,0 +1,24 @@
+"""
+   programa31.py
+   Nombre:Angel Guerra Muñoz
+   Fecha:23/02/2023
+   Descripcion:Calcula el are y volumen de un hexaedro
+""" 
+
+
+lado1 = float(input("Ingrese numero de lado1: "))
+lado2 = float(input("Ingrese numero de lado2: "))
+lado3 = float(input("Ingrese numero de lado3: "))
+lado4 = float(input("Ingrese numero de lado4: "))
+lado5 = float(input("Ingrese numero de lado5: "))
+lado6 = float(input("Ingrese numero de lado6: "))
+
+
+area = 6*lado1**2
+volumen = lado1**3
+
+print("El area del hexaedro es: ",area)
+print("El volumen del hexaedro es: ",volumen)
+
+
+

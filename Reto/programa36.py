d80b0969018b2f2acd054d29108ce192acffd59e
ANGEL GUERRA <122810612+GUERRA-ANGEL@users.noreply.github.com>
2023-02-27 03:01:15 +0000

    Convierte los galones de gasolina en pesos para cobrar a cliente

---
+++ Reto/programa36.py
@@ -0,0 +1,10 @@
+"""
+   programa36.py
+   Nombre:Angel Guerra Muñoz
+   Fecha:26/02/2023
+   Descripcion:Convierte los galones de gasolina en pesos para cobrar a cliente
+"""
+
+Num = float(input("Ingrese el numero de galones de gasolina que quiere el cliente: "))
+Total = Num*5.785*8.20
+print("El monto a cobrar es: ",Total)
